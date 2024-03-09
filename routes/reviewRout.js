204b766fe8d066af7cbb26f7ce0f16602748d98d
mariamahmed000 <mariam.memo455@gmail.com>
2024-03-09 04:37:12 +0200

    pull changes

--- routes/reviewRout.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reviewController = require("../controller/reviewController");
-
-router.route("/reveiw/:id")
-.post(reviewController.setReview);
-
-module.exports=router
